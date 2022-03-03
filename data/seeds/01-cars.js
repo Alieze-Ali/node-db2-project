1e5acadd826dd04d0149f732e38329c992db38fa
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2022-03-02 18:58:49 -0800

    added table

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,35 @@
 // STRETCH
+const cars = [
+    {
+    vin: '111111111111111',
+    make: 'toyota',
+    model: 'corolla',
+    mileage: 175000,
+    title: 'clean',
+    transmission: 'automatic',
+},
+{
+    vin: '111111111111112',
+    make: 'honda',
+    model: 'civic',
+    mileage: 10000,
+    title: 'clean',
+    
+},
+{
+    vin: '111111111111113',
+    make: 'jeep',
+    model: 'wrangler',
+    mileage: 25000,
+    title: 'salvage',
+
+},
+]
+exports.seed = function(knex) {
+    return knex('cars')
+    .truncate().then(() => {
+        return knex('cars').insert(cars)
+    })
+   
+
+}
